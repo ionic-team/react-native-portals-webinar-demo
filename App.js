526979cee82019ef222db9a077a64dc223347622
Josh Thomas <jthoms1@gmail.com>
2022-06-27 20:32:39 -0500

    Add portals to project.

--- App.js
+++ App.js
@@ -26,6 +26,26 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
+import {addPortal, register, PortalView} from '@ionic/portals-react-native';
+
+register(
+  'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiIzMDU2ZjBlNC1kYTFkLTQ1YWMtYWJjZi1hNDg3MzMyZTQwNGYifQ.0H_gnwXCL1Z-GtFCwQ3J9YrybMxQO56CYo3PFGzoueB56DMvKT4jiQhLzhDYKEE5GwlqX-r0H_qklYKg_jtMyK9QZ_-kTNWi6LyjrJTcgFVwxjz27PZaqZPoKWyJLotSIbBhN8BF5flunCGW8kWL4_nY6FUmswatPDgcvyPmOydr9InbEHHDUVvi9mGwy_G78BjDrl9bThezpGRseBTOI7KH5FUdXwH9DCZJ2RC4_ukTNKMqaKFh-OcD8KDBUIdSP8GE0quO7zL4qSINvxMMzpupTdQKf3Td5B1mvLlrS4kF_8VPoQtvB8JqMrmH2fa8f31fCiz1EV4Wkngb_5yC7w',
+);
+
+const helloPortal = {
+  // A unique name to reference later
+  name: 'hello',
+  // This is the location of your web bundle relative to the asset directory in Android and Bundle.main in iOS
+  // This will default to the name of the portal
+  startDir: 'portals/hello',
+  // Any initial state to be provided to a Portal if needed
+  initialContext: {
+    greeting: 'Hello, world!',
+  },
+};
+
+addPortal(helloPortal);
+
 const Section = ({children, title}): Node => {
   const isDarkMode = useColorScheme() === 'dark';
   return (
@@ -70,6 +90,14 @@ const App: () => Node = () => {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
+          <PortalView
+            // The name of the portal to be used in the view
+            name="hello"
+            // Set any initial context you may want to override.
+            initialContext={{greeting: 'Goodbye!'}}
+            // Setting a size is required
+            style={{flex: 1, height: 300}}
+          />
           <Section title="Step One">
             Edit <Text style={styles.highlight}>App.js</Text> to change this
             screen and then come back to see your edits.
